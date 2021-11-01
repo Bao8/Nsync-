1e636aaebdaa92d667909e53dcbe99c997ddaa92
Bao8 <yasuhiro.oba@nayutanet.jp>
2021-11-01 18:27:19 +0900

    add file

--- resources/views/dashboad.blade.php
+++ resources/views/dashboad.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <title>Dashboard Template for Bootstrap · Bootstrap</title>
+    <title>Nsync Beta</title>
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 	  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
