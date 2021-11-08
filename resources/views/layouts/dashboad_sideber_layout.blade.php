fee9344e6e8076a20f707b356379002c3027e5a0
pole-star160202 <hiranuma@nayutanet.jp>
2021-11-08 07:45:02 +0000

    サイドバー更新2

--- resources/views/layouts/dashboad_sideber_layout.blade.php
+++ resources/views/layouts/dashboad_sideber_layout.blade.php
@@ -8,33 +8,33 @@
 				<span class="sr-only">(current)</span>
 			</a>
 			<ul class="nav-sub">
-				<li class="nav-item">
-					<a class="nav-link" href="#">今日の納品</a>
+				<li>
+					<a class="nav-link" href="dashboad_home">今日の納品</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">明日の納品</a>
+				<li>
+					<a class="nav-link" href="dashboad_home">明日の納品</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">About 3</a>
+				<li>
+					<a class="nav-link" href="dashboad_home">About 3</a>
 				</li>
 			</ul>
 		</li>
-		<li class="nav-item">
+		<li>
 			<a class="nav-link" href="dashboad_restore">
 			リストア依頼<span data-feather="file"></span>
 			</a>
 		</li>
-		<li class="nav-item">
+		<li>
 			<a class="nav-link" href="dashboad_sozai">
 		  	素材管理<span data-feather="shopping-cart"></span>
 			</a>
 		</li>
-	  	<li class="nav-item">
+		<li>
 			<a class="nav-link" href="dashboad_meta">
 		  	メタ管理<span data-feather="users"></span>
 			</a>
 		</li>
-	  	<li class="nav-item">
+		<li>
 			<a class="nav-link" href="dashboad_others">
 		  	その他<span data-feather="bar-chart-2"></span>
 			</a>
