a787eab248f5759701478ec7073a18b2e9c29811
pole-star160202 <hiranuma@nayutanet.jp>
2021-11-10 06:35:20 +0000

    基本情報更新２

--- resources/views/dashboad_soozai_info.blade.php
+++ resources/views/dashboad_soozai_info.blade.php
@@ -1,10 +1,53 @@
-<form>
-  <div class="mb-3">
-    <label for="episodeNo" class="form-label">エピソードNo.</label>
-    <input type="number" class="form-control" id="episodeNo" aria-describedby="" style="width:20%;">
-  </div>
-  <div class="mb-3">
-    <label for="episodeNo" class="form-label">エピソードNo.</label>
-    <input type="number" class="form-control" id="episodeNo" aria-describedby="" style="width:20%;">
-  </div>
+<form class="row g-3">
+	<div class="col-12">
+    	<label for="contentscode" class="form-label">コンテンツコード</label>
+    	<input type="text" class="form-control" id="contentscode" aria-describedby="" value="VCJPA10000066899">
+ 	</div>
+	<div class="col-md-3">
+		<label for="episodeNo" class="form-label">エピソードNo.</label>		
+		<input type="number" class="form-control" id="episodeNo" aria-describedby="" value="1">
+	</div>
+	<div class="col-md-9">
+		<label for="episodetitle" class="form-label">エピソードタイトル</label>
+		<input type="text" class="form-control" id="episodetitle" aria-describedby="" value="3分間で、竹製耳かき">
+	</div>
+	<div class="col-12">
+		<label for="rightsource" class="form-label">権利元</label>
+		<input type="text" class="form-control" id="rightsource" aria-describedby="" value="ヴューズ">
+	</div>
+	<div class="col-md-4">
+		<label for="length" class="form-label">長さ</label>		
+		<input type="text" class="form-control" id="length" aria-describedby="" value="0:03:29">		
+	</div>
+	<div class="w-100"></div>
+	<div class="col-md-4">
+		<label for="contentstype" class="form-label">コンテンツタイプ</label>
+		<select id="contentstype" class="form-select">
+			<option selected>本編</option>
+			<option>予告編</option>
+		</select>
+	</div>
+	<div class="w-100"></div>
+	<div class="col-md-6">
+		<label for="category" class="form-label">カテゴリ</label>
+		<select id="category" class="form-select">
+			<option selected>邦画</option>
+			<option>洋画</option>
+			<option>国内ドラマ</option>
+			<option>海外ドラマ</option>
+			<option>アニメ</option>
+			<option>海外アニメ</option>
+		</select>
+	</div>
+	<div class="w-100"></div>
+	<div class="col-md-6">
+		<label for="matter" class="form-label">案件</label>
+		<select id="matter" class="form-select">
+			<option selected>Amazon</option>
+			<option>ATV</option>
+			<option>Hulu</option>
+			<option>Paravi</option>
+			<option>TVer</option>
+		</select>
+	</div>
 </form>
